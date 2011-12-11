8317d67710f502e12f4d29dd878c0a6964e3979a
Charles Amey <kc8hfi@gmail.com>
2011-12-11 15:23:25 -0500

    Added new class, DataInfo
    The DataInfo class is an object for the tree node items

---
+++ src/scratchpad/DataInfo.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2002-2011 Charles Amey
+ * 
+ * This file is part of Scratchpad.
+ * 
+ * Scratchpad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Scratchpad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Scratchpad.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package scratchpad;
+
+public class DataInfo
+{
+	public DataInfo (String n, String d)
+	{
+		name = n;
+		data = d;
+	}
+	public String toString()
+	{
+		return name;
+	}
+	private String name;
+	private String data;
+}//end datainfo
